36f1d6e82a3e274377a5f112e82fe04255b5216b
Joannis Orlandos <joannis@orlandos.nl>
2017-08-09 22:00:37 +0200

    Update dependencies

--- Tests/PumaTests/PumaTests.swift
+++ Tests/PumaTests/PumaTests.swift
@@ -3,9 +3,11 @@ import XCTest
 
 class PumaTests: XCTestCase {
     func testExample() throws {
-        let request = Request(method: .get, path: "/")
+        let request = Request(method: .get, path: "/", headers: [
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.28 (KHTML, like Gecko) Version/11.0 Safari/604.1.28"
+        ])
         
-        let response = try SyncHTTPClient.send(request, toHost: "google.com", securely: true)
+        let response = try SyncHTTPClient.send(request, to: "https://independer.nl/")
         
         print(response)
     }
